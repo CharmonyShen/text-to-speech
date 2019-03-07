d470c61f14f5d1c62401e1d52950d02fa3bb53b7
CharmonyShen <ssc530pay@icloud>
2019-03-07 16:26:27 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" style="-webkit-app-region: drag" class="font-sans flex min-h-screen">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <sidebar></sidebar>
+    <router-view/>
+  </div>
+</template>
+
+// <script>
+import Sidebar from './components/Sidebar.vue'
+
+export default {
+  components: {
+    Sidebar
+  }
+}
+</script>
+
+<style src="./assets/main.css">
